6aa93de1d037115e84eca8d640e39b875fedaa4d
Izzudin Anuar <izzudinanuar96@gmail.com>
2026-08-25 20:33:18 +1200

    Wire config into CLI with slog logging

--- cmd/nadi-agent/main.go
+++ cmd/nadi-agent/main.go
@@ -1,9 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log/slog"
+	"os"
+
+	"github.com/izzudin96/nadi-agent/internal/config"
+)
 
 var version = "dev"
 
 func main() {
-	fmt.Printf("nadi-agent starting (version %s)\n", version)
+	if err := run(); err != nil {
+		slog.Error("agent exiting", "err", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	configPath := flag.String("config", "agent.yaml", "path to agent config file")
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		return err
+	}
+
+	logger := newLogger(cfg.LogLevel)
+
+	logger.Info("agent starting",
+		"version", version,
+		"device_id", cfg.DeviceID,
+		"server_url", cfg.ServerURL,
+		"interval_seconds", cfg.IntervalSeconds,
+	)
+
+	return nil
+}
+
+func newLogger(level string) *slog.Logger {
+	var lvl slog.Level
+	switch level {
+	case "debug":
+		lvl = slog.LevelDebug
+	case "warn":
+		lvl = slog.LevelWarn
+	case "error":
+		lvl = slog.LevelError
+	default:
+		lvl = slog.LevelInfo
+	}
+	return slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: lvl}))
 }
